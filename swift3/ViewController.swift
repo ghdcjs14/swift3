0fb04a4476c6732aca59908168a62e7d5d09ab9f
hong <black1990-14@hanmail.net>
2017-03-07 20:44:07 +0900

    Initial Commit

---
+++ swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift3
+//
+//  Created by ChoiHong Sun on 2017. 3. 7..
+//  Copyright © 2017년 stranger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
